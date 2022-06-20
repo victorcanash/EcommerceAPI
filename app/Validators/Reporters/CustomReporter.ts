4886979b21d25a2718ddbd3555076fd20383c2df
Victor Canas <victorcanas972@gmail.com>
2022-06-20 16:41:57 +0200

    Add custom reporter from validators

---
+++ app/Validators/Reporters/CustomReporter.ts
@@ -0,0 +1,80 @@
+import {
+  ValidationException,
+  MessagesBagContract,
+  ErrorReporterContract,
+} from '@ioc:Adonis/Core/Validator'
+
+import { ValidationErrorResponse } from 'App/Exceptions/types'
+
+/**
+ * The shape of an individual error
+ */
+export type ErrorNode = {
+  message: string
+  field: string
+}
+
+export class CustomReporter implements ErrorReporterContract<{ errors: ErrorNode[] }> {
+  public hasErrors = false
+
+  /**
+   * Tracking reported errors
+   */
+  private errors: ErrorNode[] = []
+
+  constructor(private messages: MessagesBagContract, private bail: boolean) {}
+
+  /**
+   * Invoked by the validation rules to
+   * report the error
+   */
+  public report(
+    pointer: string,
+    rule: string,
+    message: string,
+    arrayExpressionPointer?: string,
+    args?: any
+  ) {
+    /**
+     * Turn on the flag
+     */
+    this.hasErrors = true
+
+    /**
+     * Use messages bag to get the error message. The messages
+     * bag also checks for the user-defined error messages and
+     * hence must always be used
+     */
+    const errorMessage = this.messages.get(pointer, rule, message, arrayExpressionPointer, args)
+
+    /**
+     * Track error message
+     */
+    this.errors.push({ message: errorMessage, field: pointer })
+
+    /**
+     * Bail mode means stop validation on the first
+     * error itself
+     */
+    if (this.bail) {
+      throw this.toError()
+    }
+  }
+
+  /**
+   * Converts validation failures to an exception
+   */
+  public toError() {
+    throw new ValidationException(false, this.toJSON())
+  }
+
+  /**
+   * Get error messages as JSON
+   */
+  public toJSON() {
+    return {
+      code: 422,
+      errors: this.errors,
+    } as ValidationErrorResponse
+  }
+}
