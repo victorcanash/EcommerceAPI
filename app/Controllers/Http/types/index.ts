ce4d3b6d52c2d4157464fdffc3c83ae5eba67656
Victor Canas <victorcanas972@gmail.com>
2022-06-22 16:44:36 +0200

    Products responses types

--- app/Controllers/Http/types/index.ts
+++ app/Controllers/Http/types/index.ts
@@ -1,4 +1,8 @@
 import User from 'App/Models/User'
+import Product from 'App/Models/Product'
+import ProductCategory from 'App/Models/ProductCategory'
+import ProductDiscount from 'App/Models/ProductDiscount'
+import ProductInventory from 'App/Models/ProductInventory'
 
 /**
  * Basic JSON response for Controllers
@@ -34,6 +38,86 @@ export type UsersResponse = {
   code: number
   message: string
   users: User[]
+  totalPages: number
+  currentPage: number
+}
+
+/**
+ * Product JSON response for Controllers
+ */
+export type ProductResponse = {
+  code: number
+  message: string
+  product: Product
+}
+
+/**
+ * Products JSON response for Controllers
+ */
+export type ProductsResponse = {
+  code: number
+  message: string
+  products: Product[]
+  totalPages: number
+  currentPage: number
+}
+
+/**
+ * Product category JSON response for Controllers
+ */
+export type PCategoryResponse = {
+  code: number
+  message: string
+  productCategory: ProductCategory
+}
+
+/**
+ * Product categories JSON response for Controllers
+ */
+export type PCategoriesResponse = {
+  code: number
+  message: string
+  productCategories: ProductCategory[]
+  totalPages: number
+  currentPage: number
+}
+
+/**
+ * Product discount JSON response for Controllers
+ */
+export type PDiscountResponse = {
+  code: number
+  message: string
+  productDiscount: ProductDiscount
+}
+
+/**
+ * Product discounts JSON response for Controllers
+ */
+export type PDiscountsResponse = {
+  code: number
+  message: string
+  productDiscounts: ProductDiscount[]
+  totalPages: number
+  currentPage: number
+}
+
+/**
+ * Product inventory JSON response for Controllers
+ */
+export type PInventoryResponse = {
+  code: number
+  message: string
+  productInventory: ProductInventory
+}
+
+/**
+ * Product inventories JSON response for Controllers
+ */
+export type PInventoriesResponse = {
+  code: number
+  message: string
+  productInventories: ProductInventory[]
   totalPages: number
   currentPage: number
 }
