dd3033d48a5cec9cdedb848751c72870a5970675
Victor Canas <victorcanas972@gmail.com>
2022-06-28 15:50:13 +0200

    Additional info of user address nullable

--- database/migrations/1655998729985_user_addresses.ts
+++ database/migrations/1655998729985_user_addresses.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
       table.increments('id').primary()
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.string('address_line', 255).notNullable()
-      table.string('additional_info', 255).defaultTo('').notNullable()
+      table.string('additional_info', 255).nullable()
       table.string('postal_code', 255).notNullable()
       table.string('locality', 255).notNullable()
       table.string('administrative_area', 255).notNullable()
