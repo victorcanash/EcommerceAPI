f487b3e6e847643dcf793ca5af3d69c5d10e4a25
Victor Canas <victorcanas972@gmail.com>
2022-08-13 16:01:10 +0200

    On login and get logged user preload inventory from cart items

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -19,6 +19,7 @@ export default class AuthController {
       .preload('cart', (query) => {
         query.preload('items', (query) => {
           query.preload('product')
+          query.preload('inventory')
         })
       })
       .first()
@@ -67,6 +68,7 @@ export default class AuthController {
       .preload('cart', (query) => {
         query.preload('items', (query) => {
           query.preload('product')
+          query.preload('inventory')
         })
       })
       .first()
