db656ad038cbc20939570c6d28db6e83d23929bb
VictorCanas <victorcanas972@gmail.com>
2022-12-22 13:11:55 +0100

    testing hooks product base model

--- app/Models/ProductBaseModel.ts
+++ app/Models/ProductBaseModel.ts
@@ -3,9 +3,12 @@ import {
   belongsTo,
   BelongsTo,
   beforeFetch,
+  beforeFind,
   afterDelete,
+  beforeCreate,
   ModelQueryBuilderContract,
 } from '@ioc:Adonis/Lucid/Orm'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 import AppBaseModel from 'App/Models/AppBaseModel'
 import LocalizedText from 'App/Models/LocalizedText'
@@ -28,13 +31,28 @@ export default class ProductBaseModel extends AppBaseModel {
   public description: BelongsTo<typeof LocalizedText>
 
   @beforeFetch()
-  public static async loadData(query: ModelQueryBuilderContract<typeof ProductBaseModel>) {
+  public static async loadDataOnFetch(query: ModelQueryBuilderContract<typeof ProductBaseModel>) {
+    Logger.error('beforeFetch')
     query.preload('name').preload('description')
+  }
+
+  @beforeFind()
+  public static async loadDataOnFind(query: ModelQueryBuilderContract<typeof ProductBaseModel>) {
+    Logger.error('beforeFind')
+    query.preload('name').preload('description')
+  }
+
+  @beforeCreate()
+  public static async loadDataOnCreate(model: ProductBaseModel) {
+    Logger.error('beforeCreate')
+    await model.load('name')
+    await model.load('description')
   }
 
   @afterDelete()
   public static async deleteLocalizedTexts(model: ProductBaseModel) {
-    model.name.delete()
-    model.description.delete()
+    Logger.error(JSON.stringify(model))
+    await model.name.delete()
+    await model.description.delete()
   }
 }
