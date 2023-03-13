b09bd9688b20600ef3408d34d0ecb9f0ea697fa3
victorcanas <victorcanas972@gmail.com>
2023-03-13 17:20:18 +0100

    fixing dispay google api errors

--- app/Services/GoogleAPIService.ts
+++ app/Services/GoogleAPIService.ts
@@ -10,34 +10,31 @@ export default class GoogleAPIService {
   private jwtClient: JWT
 
   constructor() {
-    this.jwtClient = new google.auth.JWT(
-      Env.get('GOOGLE_API_CLIENT_EMAIL', ''),
-      undefined,
-      Env.get('GOOGLE_API_PRIVATE_KEY', ''),
-      ['https://www.googleapis.com/auth/indexing'],
-      undefined
-    )
+    this.jwtClient = new google.auth.JWT({
+      email: Env.get('GOOGLE_API_CLIENT_EMAIL', ''),
+      key: Env.get('GOOGLE_API_PRIVATE_KEY', ''),
+      scopes: ['https://www.googleapis.com/auth/indexing'],
+    })
   }
 
   private async getAccessToken() {
-    return new Promise<{ googleAccessToken: string }>((resolve, _reject) => {
+    return new Promise<{ googleAccessToken: string }>((resolve, reject) => {
       this.jwtClient.authorize((error, tokens) => {
         if (error || !tokens?.access_token) {
-          throw new InternalServerException(
-            `Error getting Google API access token: ${
-              error?.message ? error.message : 'Empty access token'
-            }`
-          )
+          reject(error ? error : new Error('Empty access token'))
+        } else {
+          resolve({
+            googleAccessToken: tokens.access_token,
+          })
         }
-        resolve({
-          googleAccessToken: tokens.access_token,
-        })
       })
     })
   }
 
   private async getAxiosOptions() {
-    let accessToken = await this.getAccessToken()
+    let accessToken = await this.getAccessToken().catch((error) => {
+      throw new InternalServerException(`Error getting Google API access token: ${error?.message}`)
+    })
     const options: AxiosRequestConfig = {
       headers: {
         'Accept': 'application/json',
