68860b37a34f20c641aaf4f58678f2cde5e0a36a
victorcanas <victorcanas972@gmail.com>
2023-03-14 18:10:42 +0100

    disable shipping set in paypal checkout

--- app/Services/PaypalService.ts
+++ app/Services/PaypalService.ts
@@ -237,6 +237,13 @@ export default class PaypalService {
           },
         },
       ],
+      payment_source: {
+        paypal: {
+          experience_context: {
+            shipping_preference: 'SET_PROVIDED_ADDRESS',
+          },
+        },
+      },
       /*payment_source: {
         card: {
           attributes: remember
