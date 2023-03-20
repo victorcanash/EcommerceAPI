3ae2c75d7637160850a29d1558f0d36059e429f7
victorcanas <victorcanas972@gmail.com>
2023-03-20 11:43:26 +0100

    redirect uri google oauth2

--- app/Services/GoogleService.ts
+++ app/Services/GoogleService.ts
@@ -12,7 +12,7 @@ export default class GoogleService {
   private jwtClient: JWT
   private oauth2Client: OAuth2Client
 
-  constructor(/*oauth2RedirectUrl = ''*/) {
+  constructor(oauth2RedirectUrl?: string) {
     this.jwtClient = new google.auth.JWT(
       Env.get('GOOGLE_AUTH_CLIENT_EMAIL', ''),
       undefined,
@@ -23,8 +23,7 @@ export default class GoogleService {
     this.oauth2Client = new google.auth.OAuth2(
       Env.get('GOOGLE_OAUTH_CLIENT_ID', ''),
       Env.get('GOOGLE_OAUTH_CLIENT_SECRET', ''),
-      'https://ecommerce-vc-api-eqhxa.ondigitalocean.app/api/auth/login/google'
-      //oauth2RedirectUrl
+      oauth2RedirectUrl || ''
     )
   }
 
