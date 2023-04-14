28d85840382077433e806ef4603e512147900782
Victor Canas <victorcanas972@gmail.com>
2023-04-14 10:43:47 +0200

    fix products index admin route

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -29,7 +29,8 @@ export default class ProductsController {
     const categoryName = validatedFilterData.categoryName || null
     const adminData = validatedFilterData.adminData || false
 
-    const isAuthAdmin = await UsersService.isAuthAdmin(auth)
+    const validApiToken = await auth.use('api').check()
+    const isAuthAdmin = !validApiToken ? false : await UsersService.isAuthAdmin(auth)
     if (adminData && !isAuthAdmin) {
       throw new PermissionException('You need to be an admin to get admin data')
     }
